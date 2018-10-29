c37920f53e023bf1f14a996abf9131b988a23e5a
Martial Geoffre-Rouland <martial@screen-club.com>
2018-10-29 12:04:34 +0100

    Updated touch manager

---
+++ SharedSrc/TouchDrawManager.cpp
@@ -0,0 +1,8 @@
+//
+//  TouchDrawManager.cpp
+//  TouchDrawTest
+//
+//  Created by Martial Geoffre-Rouland on 29/10/2018.
+//
+
+#include "TouchDrawManager.hpp"
---
+++ SharedSrc/TouchDrawManager.hpp
@@ -0,0 +1,13 @@
+//
+//  TouchDrawManager.hpp
+//  TouchDrawTest
+//
+//  Created by Martial Geoffre-Rouland on 29/10/2018.
+//
+
+#ifndef TouchDrawManager_hpp
+#define TouchDrawManager_hpp
+
+#include <stdio.h>
+
+#endif /* TouchDrawManager_hpp */
